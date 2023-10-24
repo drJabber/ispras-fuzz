557d573ccb36f0bdece813fb43225a09e0c42129
DVSutyagin <dvsutyagin@greenatom.ru>
2023-10-24 13:22:13 +0300

    fix

--- .scripts/setup_tests.sh
+++ .scripts/setup_tests.sh
@@ -8,7 +8,7 @@ wget https://raw.githubusercontent.com/richgel999/random_pngs/main/random_pngs.7
 7z x ./test/png/test.7z -o./test/png -y
 rm ./test/png/test.7z
 
-test_pngs = (./test/png/*.png)
+test_pngs=(./test/png/*.png)
 for png in ${test_pngs[@]:0:20}; 
 do 
     ./png2bin -i $png -o ${png}".bin" -p 0; 
