6d557eaa78a0e9abb92f96536113048c7c6de132
DVSutyagin <dvsutyagin@greenatom.ru>
2023-10-24 15:00:55 +0300

    tests

--- .scripts/setup_tests.sh
+++ .scripts/setup_tests.sh
@@ -8,18 +8,22 @@ wget https://raw.githubusercontent.com/richgel999/random_pngs/main/random_pngs.7
 7z x ./test/png/test.7z -o./test/png -y
 rm ./test/png/test.7z
 
+echo "test png2bin"
+
 test_pngs=(./test/png/*.png)
 for png in ${test_pngs[@]:0:20}; 
 do 
-    ./png2bin -i $png -o ${png}".bin" -p 0; 
+    ./png2bin -i $png -o ${png}".bin" -p 0 || true; 
 done
 
 /tmp/.scripts/radamsa --generators random -n 20 -o ./test/bin/test-%02n.bin
 
+echo "test bin2png"
+
 test_bins=(./test/bin/*.bin)
 for bin in ${test_bins}; 
 do 
-    ./bin2png -i $bin -o ${bin}".png" -p $(($RANDOM % 256)); 
+    ./bin2png -i $bin -o ${bin}".png" -p $(($RANDOM % 256)) || true; 
 done
 
 
