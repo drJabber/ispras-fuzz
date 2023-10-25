6772626ce4f5c7592c1453cf65eb5b83a6590de2
DVSutyagin <dvsutyagin@greenatom.ru>
2023-10-25 17:21:49 +0300

    readme and extra tests

--- .scripts/setup_tests.sh
+++ .scripts/setup_tests.sh
@@ -25,8 +25,7 @@ do
     LLVM_PROFILE_FILE="./.coverage/bin2png-%p.profraw" ./bin2png -i $bin -o ${bin}".png" -p $(($RANDOM % 300)) || true; # 300>256, so paths with -p errors also will be covered
 done
 
-ls ./.coverage/*profraw
-test_pngs=`find ./.coverage/*profraw -maxdepth 1 -type f -not -path '*/\.*'`
+test_pngs=`find ./.coverage/*profraw -maxdepth 1 -type f `
 echo $test_pngs
 
 llvm-profdata merge -sparse $test_pngs -o ./.coverage/imgify.profdata
