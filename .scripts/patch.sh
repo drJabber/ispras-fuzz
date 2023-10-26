94780e504815aa56676f829632f43b729178457d
DVSutyagin <dvsutyagin@greenatom.ru>
2023-10-26 16:11:15 +0300

    readme and extra tests

--- .scripts/patch.sh
+++ .scripts/patch.sh
@@ -34,15 +34,21 @@
              echo "patch to afl"
              temp_file_name="$(mktemp /tmp/foo.XXXXXXXXX)" && \
                 cat ./Makefile | \
-                awk -v replacement="\tafl-clang -o $@ -c imgify.c \$(CFLAGS);" 'NR==18{$0=replacement}{print}'  > $temp_file_name && \
+                awk -v replacement="\tafl-clang -o \$@ -c imgify.c \$(CFLAGS);" 'NR==18{\$0=replacement}{print}'  > $temp_file_name && \
+                mv -f $temp_file_name ./Makefile
+
+             echo "patch to afl"
+             temp_file_name="$(mktemp /tmp/foo.XXXXXXXXX)" && \
+                cat ./Makefile | \
+                awk -v replacement="\tafl-clang -o \$@ -c imgify.c \$(CFLAGS);" 'NR==21{\$0=replacement}{print}'  > $temp_file_name && \
                 mv -f $temp_file_name ./Makefile
 
              temp_file_name="$(mktemp /tmp/foo.XXXXXXXXX)" && \
                 cat ./Makefile | \
-                awk -v replacement='\tafl-clang -o $@ bin2png.c $^ \$(CFLAGS) \$(LDFLAGS)' 'NR==21{$0=replacement}{print}'  > $temp_file_name && \
+                awk -v replacement='\tafl-clang -o \$@ bin2png.c $^ \$(CFLAGS) \$(LDFLAGS)' 'NR==24{\$0=replacement}{print}'  > $temp_file_name && \
                 mv -f $temp_file_name ./Makefile
 
              temp_file_name="$(mktemp /tmp/foo.XXXXXXXXX)" && \
                 cat ./Makefile | \
-                awk -v replacement='\tafl-clang -o $@ bin2png.c $^ \$(CFLAGS) \$(LDFLAGS)' 'NR==27{$0=replacement}{print}'  > $temp_file_name && \
+                awk -v replacement='\tafl-clang -o \$@ bin2png.c $^ \$(CFLAGS) \$(LDFLAGS)' 'NR==27{\$0=replacement}{print}'  > $temp_file_name && \
                 mv -f $temp_file_name ./Makefile
